2e893a8218c20f05396ca384fa33033492d26494
DavidZZGi <zzdaviddev@gmail.com>
2024-01-04 17:19:02 -0500

    removing flexible widgets

--- lib/widgets/data_table_widget.dart
+++ lib/widgets/data_table_widget.dart
@@ -7,62 +7,61 @@ class VasselTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
-      child: Flexible(
-        child: DataTable(
-          dataTextStyle: const TextStyle(
-            fontSize: 12,
-          ),
-          columnSpacing: 40,
-
-          showCheckboxColumn: false, // Oculta la columna de checkbox
-
-          columns: const [
-            DataColumn(label: Text('Vessel Type')),
-            DataColumn(label: Icon(Icons.local_shipping)),
-            DataColumn(label: Icon(Icons.checkroom)),
-            DataColumn(label: Icon(Icons.directions_boat)),
-          ],
-          rows: const [
-            DataRow(cells: [
-              DataCell(
-                  Text('Ocean Voyager', style: TextStyle(color: Colors.black))),
-              DataCell(Icon(Icons.local_shipping)),
-              DataCell(Icon(Icons.checkroom)),
-              DataCell(Icon(Icons.directions_boat)),
-
-              // Add other relevant cells here
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Harbor Explorer',
-                  style: TextStyle(color: Colors.black))),
-              DataCell(Icon(Icons.local_shipping)),
-              DataCell(Icon(Icons.checkroom)),
-              DataCell(Icon(Icons.directions_boat)),
-
-              // Add other relevant cells here
-            ]),
-            DataRow(cells: [
-              DataCell(
-                  Text('Sea Guardian', style: TextStyle(color: Colors.black))),
-              DataCell(Icon(Icons.local_shipping)),
-              DataCell(Icon(Icons.checkroom)),
-              DataCell(Icon(Icons.directions_boat)),
-
-              // Add other relevant cells here
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Coastal Pioneer',
-                  style: TextStyle(color: Colors.black))),
-              DataCell(Icon(Icons.local_shipping)),
-              DataCell(Icon(Icons.checkroom)),
-              DataCell(Icon(Icons.directions_boat)),
-
-              // Add other relevant cells here
-            ]),
-
-            // Add more rows as needed
-          ],
+      child: DataTable(
+        dataTextStyle: const TextStyle(
+          fontSize: 12,
         ),
+
+        columnSpacing: 40,
+
+        showCheckboxColumn: false, // Oculta la columna de checkbox
+
+        columns: const [
+          DataColumn(label: Text('Vessel Type')),
+          DataColumn(label: Icon(Icons.local_shipping)),
+          DataColumn(label: Icon(Icons.checkroom)),
+          DataColumn(label: Icon(Icons.directions_boat)),
+        ],
+        rows: const [
+          DataRow(cells: [
+            DataCell(
+                Text('Ocean Voyager', style: TextStyle(color: Colors.black))),
+            DataCell(Icon(Icons.local_shipping)),
+            DataCell(Icon(Icons.checkroom)),
+            DataCell(Icon(Icons.directions_boat)),
+
+            // Add other relevant cells here
+          ]),
+          DataRow(cells: [
+            DataCell(
+                Text('Harbor Explorer', style: TextStyle(color: Colors.black))),
+            DataCell(Icon(Icons.local_shipping)),
+            DataCell(Icon(Icons.checkroom)),
+            DataCell(Icon(Icons.directions_boat)),
+
+            // Add other relevant cells here
+          ]),
+          DataRow(cells: [
+            DataCell(
+                Text('Sea Guardian', style: TextStyle(color: Colors.black))),
+            DataCell(Icon(Icons.local_shipping)),
+            DataCell(Icon(Icons.checkroom)),
+            DataCell(Icon(Icons.directions_boat)),
+
+            // Add other relevant cells here
+          ]),
+          DataRow(cells: [
+            DataCell(
+                Text('Coastal Pioneer', style: TextStyle(color: Colors.black))),
+            DataCell(Icon(Icons.local_shipping)),
+            DataCell(Icon(Icons.checkroom)),
+            DataCell(Icon(Icons.directions_boat)),
+
+            // Add other relevant cells here
+          ]),
+
+          // Add more rows as needed
+        ],
       ),
     );
   }
--- lib/widgets/jobs_state_widget.dart
+++ lib/widgets/jobs_state_widget.dart
@@ -8,44 +8,42 @@ class JobsStateWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
-      child: Flexible(
-        child: DataTable(
-          dataTextStyle: const TextStyle(
-            fontSize: 12,
-          ),
-
-          showCheckboxColumn: false, // Oculta la columna de checkbox
-
-          columns: [
-            const DataColumn(label: Text('Active Jobs:')),
-            DataColumn(
-                label: NumberCard(
-              number: 80500,
-            )),
-          ],
-          rows: [
-            DataRow(cells: [
-              const DataCell(
-                  Text('Pending Jobs', style: TextStyle(color: Colors.black))),
-              DataCell(NumberCard(
-                number: 20500,
-              )),
-
-              // Add other relevant cells here
-            ]),
-            DataRow(cells: [
-              const DataCell(Text('Cancelled Jobs',
-                  style: TextStyle(color: Colors.black))),
-              DataCell(NumberCard(
-                number: 10000,
-              )),
-
-              // Add other relevant cells here
-            ]),
-
-            // Add more rows as needed
-          ],
+      child: DataTable(
+        dataTextStyle: const TextStyle(
+          fontSize: 12,
         ),
+
+        showCheckboxColumn: false, // Oculta la columna de checkbox
+
+        columns: [
+          const DataColumn(label: Text('Active Jobs:')),
+          DataColumn(
+              label: NumberCard(
+            number: 80500,
+          )),
+        ],
+        rows: [
+          DataRow(cells: [
+            const DataCell(
+                Text('Pending Jobs', style: TextStyle(color: Colors.black))),
+            DataCell(NumberCard(
+              number: 20500,
+            )),
+
+            // Add other relevant cells here
+          ]),
+          DataRow(cells: [
+            const DataCell(
+                Text('Cancelled Jobs', style: TextStyle(color: Colors.black))),
+            DataCell(NumberCard(
+              number: 10000,
+            )),
+
+            // Add other relevant cells here
+          ]),
+
+          // Add more rows as needed
+        ],
       ),
     );
   }
